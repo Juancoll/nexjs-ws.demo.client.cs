7b7201f9f0004d5d17fda143c3fa0daa8f4743dd
juancoll <juan.coll@nex-group.io>
2020-08-19 18:03:13 +0200

    first commit

---
+++ src/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace demo.wsclient
+{
+    public partial class MainWindow : Window
+    {       
+        #region [ constructor ]
+        public MainWindow()
+        {
+            InitializeComponent();    
+        }
+        #endregion
+    }
+}
---
+++ src/controls/WSConnection.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace demo.wsclient.src.controls
+{
+    /// <summary>
+    /// Interaction logic for WSConnection.xaml
+    /// </summary>
+    public partial class WSConnection : UserControl
+    {
+        public WSConnection()
+        {
+            InitializeComponent();
+        }
+
+        private void _uiButtonConnect_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            try
+            {
+                Console.WriteLine("[ui] Connect");
+                Context.Instance.wsapi.Ws.Connect("ws://localhost:3000/wsapi", "/");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[error] {ex.Message}");
+            }
+        }
+
+        private void _uiButtonDisconnect_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            try
+            {
+                Console.WriteLine("[ui] Disconnect");
+                Context.Instance.wsapi.Ws.Disconnect();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[error] {ex.Message}");
+            }
+        }
+
+        private async void _uiButtonRegister_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            try
+            {
+                Console.WriteLine("[ui] auth.register");
+                await Context.Instance.wsapi.Auth.Register(new { email = "admin@nex-group.io", password = "123456" });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[error] {ex.Message}");
+            }
+        }
+
+        private async void _uiButtonLogin_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            try
+            {
+                Console.WriteLine("[ui] auth.register");
+                await Context.Instance.wsapi.Auth.Login(new { email = "admin@nex-group.io", password = "123456" });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[error] {ex.Message}");
+            }
+        }
+
+        private async void _uiButtonLogout_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            try
+            {
+                Console.WriteLine("[ui] auth.register");
+                await Context.Instance.wsapi.Auth.Logout();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[error] {ex.Message}");
+            }
+        }
+    }
+}
